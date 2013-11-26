c28d5389a3e1aff2689c3e1559b2c959387b0d28
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-25 22:34:25 -0500

    New class: PatternViewCell

---
+++ UICollectionViewDemo/PatternViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PatternViewCell.h
+//  UICollectionViewDemo
+//
+//  Created by Alain Malenfant on 11/25/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PatternViewCell : UICollectionViewCell
+
+@property (nonatomic, weak) IBOutlet UIImageView *patternImageView;
+@property (nonatomic, weak) IBOutlet UILabel *patternLabel;
+
+@end
---
+++ UICollectionViewDemo/PatternViewCell.m
@@ -0,0 +1,31 @@
+//
+//  PatternViewCell.m
+//  UICollectionViewDemo
+//
+//  Created by Alain Malenfant on 11/25/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import "PatternViewCell.h"
+
+@implementation PatternViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
